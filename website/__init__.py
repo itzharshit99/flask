c9c184e742a1a7dde064c413f5c1e952bc820aa7
Harshit Jain <iharshitjain99@gmail.com>
2025-05-10 11:21:50 +0530

    setted up route in views and auth

--- website/__init__.py
+++ website/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'sghsvhgvhgh'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/auth')
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,13 @@
 from flask import Blueprint
 
 auth = Blueprint('auth',__name__)
+
+@auth.route("/login")
+def login():
+    return "login"
+@auth.route("/logout")
+def logout():
+    return "logout"
+@auth.route("/sign-up")
+def signUp():
+    return "sign-up"
